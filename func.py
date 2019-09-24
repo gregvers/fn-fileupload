fa3c3ea2449b262c012bb4e39b7cc90a041e9a32
gregvers <gregvers@gmail.com>
2019-09-23 17:20:14 -0700

    initial

--- func.py
+++ func.py
@@ -12,6 +12,7 @@ OBJECT_STORAGE_URL = "https://objectstorage.us-phoenix-1.oraclecloud.com"
 UPLOAD_BUCKET = 'gregbucket'
 
 def handler(ctx, data: io.BytesIO=None):
+    # get 2 arguments from invoke: name and sessionid, for example {"name": "Greg", "sessionid":"S101"}
     try:
         body = json.loads(data.getvalue())
         name = body.get("name")
@@ -19,8 +20,7 @@ def handler(ctx, data: io.BytesIO=None):
     except (Exception, ValueError) as ex:
         logging.error(str(ex))
 
-
-
+    # create a PAR for bucket defined in UPLOAD_BUCKET
     signer = oci.auth.signers.get_resource_principals_signer()
     object_storage = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
     time_expires = datetime.utcnow() + timedelta(minutes=5)
@@ -29,11 +29,13 @@ def handler(ctx, data: io.BytesIO=None):
     par = object_storage.create_preauthenticated_request(namespace, UPLOAD_BUCKET, par_details)
     par_url = OBJECT_STORAGE_URL + par.data.access_uri
 
+    # prepare the HTML page to return to the user
     var_dict = { 'name':name, 'sessionid':sessionid, 'PAR_URL':par_url }
     html_file = open( 'fileupload.html' )
     templ_response = Template( html_file.read() )
     resp = templ_response.substitute(var_dict)
 
+    # return the response in HTML
     return response.Response(
         ctx,
         response_data=resp,
