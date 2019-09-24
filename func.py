70e51f3a98f4e1985e1f73a668ec1772f7b5ca9a
gregvers <gregvers@gmail.com>
2019-09-23 17:15:30 -0700

    initial

---
+++ func.py
@@ -0,0 +1,41 @@
+import io
+import json
+import logging
+from string import Template
+import oci
+from oci.object_storage.models import CreatePreauthenticatedRequestDetails
+from datetime import datetime, timedelta
+
+from fdk import response
+
+OBJECT_STORAGE_URL = "https://objectstorage.us-phoenix-1.oraclecloud.com"
+UPLOAD_BUCKET = 'gregbucket'
+
+def handler(ctx, data: io.BytesIO=None):
+    try:
+        body = json.loads(data.getvalue())
+        name = body.get("name")
+        sessionid = body.get("sessionid")
+    except (Exception, ValueError) as ex:
+        logging.error(str(ex))
+
+
+
+    signer = oci.auth.signers.get_resource_principals_signer()
+    object_storage = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
+    time_expires = datetime.utcnow() + timedelta(minutes=5)
+    par_details = CreatePreauthenticatedRequestDetails(name="par-"+name+"-"+sessionid, access_type='AnyObjectWrite', time_expires=time_expires )
+    namespace = object_storage.get_namespace().data
+    par = object_storage.create_preauthenticated_request(namespace, UPLOAD_BUCKET, par_details)
+    par_url = OBJECT_STORAGE_URL + par.data.access_uri
+
+    var_dict = { 'name':name, 'sessionid':sessionid, 'PAR_URL':par_url }
+    html_file = open( 'fileupload.html' )
+    templ_response = Template( html_file.read() )
+    resp = templ_response.substitute(var_dict)
+
+    return response.Response(
+        ctx,
+        response_data=resp,
+        headers={"Content-Type": "text/html"}
+    )
